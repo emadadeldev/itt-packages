c69c593b12097200d5f3cd10ffd94c94ffd9a384
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-11-12 04:44:22 +0200

    Update install.ps1

--- automation/aimp/install.ps1
+++ automation/aimp/install.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
     fileType       = 'exe'
     url            = 'https://www.aimp.ru/?do=download.file&id=4'
     url64          = 'https://www.aimp.ru/?do=download.file&id=3'
-    installerName  = 'aimp_5.40.2655_w32.exe'
-    installerName64 = 'aimp_5.40.2655_w64.exe'
+    installerName  = 'aimp_5.40.2696_w32.exe'
+    installerName64 = 'aimp_5.40.2696_w64.exe'
     silentArgs     = '/AUTO /SILENT /RUN'
     validExitCodes = @(0, 1223)
 }
